be555363dcab7a4bbae54eb993da763615874c72
Paul Clay <pclaycs@gmail.com>
2023-12-11 02:30:49 +1100

    Added CommandHandler and adjusted code to allow for interactions

--- MrPorker/Program.cs
+++ MrPorker/Program.cs
@@ -1,6 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Discord.WebSocket;
+using Discord.Interactions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using MrPorker;
+using MrPorker.Configs;
+//using MrPorker.Services;
+using System;
+using System.Threading.Tasks;
+using MrPorker.Services;
 
 Console.WriteLine("Hello, World!");
-var bot = new Bot();
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var services = ConfigureServices(configuration);
+
+var serviceProvider = services.BuildServiceProvider();
+var bot = serviceProvider.GetRequiredService<Bot>();
 await bot.RunAsync();
+
+IServiceCollection ConfigureServices(IConfiguration configuration)
+{
+    var botConfig = configuration.Get<BotConfig>() ?? throw new InvalidOperationException("Bot configuration not found.");
+
+    return new ServiceCollection()
+        .AddSingleton(new DiscordSocketClient())
+        .AddSingleton<InteractionService>()
+        .AddSingleton<CommandHandler>()
+        .AddSingleton(botConfig)
+        .AddSingleton<Bot>();
+}
---
+++ MrPorker/Services/CommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using MrPorker.Configs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrPorker.Services
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _interactionService;
+        private readonly IServiceProvider _services;
+        private readonly BotConfig _botConfig;
+
+        public CommandHandler(DiscordSocketClient client, InteractionService interactionService, IServiceProvider services, BotConfig botConfig)
+        {
+            _client = client;
+            _interactionService = interactionService;
+            _services = services;
+            _botConfig = botConfig;
+        }
+
+        public async Task InitializeAsync()
+        {
+            _client.Ready += OnReadyAsync;
+            _client.InteractionCreated += OnInteractionCreatedAsync;
+
+            await _interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task OnReadyAsync()
+        {
+            // If you're using guild-specific commands, use the guild ID instead of null
+            await _interactionService.RegisterCommandsToGuildAsync(_botConfig.GuildHideoutId);
+            if (_client.GetChannel(_botConfig.ChannelGeneralId) is IMessageChannel channel)
+            {
+                await channel.SendMessageAsync("Hello fuckers im new and improved");
+            }
+            else
+            {
+                Console.WriteLine("Channel not found.");
+            }
+        }
+
+        private async Task OnInteractionCreatedAsync(SocketInteraction interaction)
+        {
+            var ctx = new SocketInteractionContext(_client, interaction);
+            await _interactionService.ExecuteCommandAsync(ctx, _services);
+        }
+    }
+}
