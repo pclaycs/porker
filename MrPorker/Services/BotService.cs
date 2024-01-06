0bedc042243c13673960bab972bf6ac3a116d252
Paul Clay <pclaycs@gmail.com>
2024-01-06 22:45:43 +1100

    Remove timezone garbage! Nonsense! I don't want that in my application!

--- MrPorker/Services/BotService.cs
+++ MrPorker/Services/BotService.cs
@@ -2,8 +2,6 @@
 using Discord.Interactions;
 using Discord.WebSocket;
 using MrPorker.Configs;
-using NodaTime;
-using System.Globalization;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
@@ -13,10 +11,10 @@ namespace MrPorker.Services
     {
         public async Task RunAsync()
         {
-
             client.Ready += OnReadyAsync;
             client.InteractionCreated += OnInteractionCreatedAsync;
             client.MessageReceived += OnMessageReceivedAsync;
+
             await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), services);
 
             await client.LoginAsync(TokenType.Bot, config.BotToken);
@@ -45,57 +43,13 @@ namespace MrPorker.Services
             var twitterLink = ReplaceTwitterLinks(message.Content);
             if (twitterLink != null)
                 await message.Channel.SendMessageAsync(twitterLink);
-
-            var extractedTime = ExtractTimeFromString(message.Content);
-            if (extractedTime != null)
-                await message.Channel.SendMessageAsync(ConvertAndDisplayTimeForUsers(extractedTime ?? new DateTime()));
-            
         }
 
-        #region Twitter Embeds
         private string? ReplaceTwitterLinks(string message)
         {
             var match = Regex.Match(message, config.TwitterLinkRegex);
             return match.Success ? $"{config.TwitterLinkReplacementHost}{match.Groups[3].Value}" : null;
         }
-        #endregion
-
-
-        #region Timezone Conversions
-        private static DateTime? ExtractTimeFromString(string input)
-        {
-            var matches = Regex.Matches(input, @"\d{1,2}(:\d{2})?\s?(am|pm|AM|PM)?");
-            foreach (Match match in matches)
-            {
-                string time = match.Value.Replace(" ", ""); // Remove any spaces
-                if (DateTime.TryParseExact(time, new[] { "htt", "h:mmtt", "HHmm", "H:mm", "hmm", "h:mm", "HH:mm", "H:mm" }, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime))
-                {
-                    return dateTime;
-                }
-            }
-
-            return null;
-        }
-
-        private static string ConvertAndDisplayTimeForUsers(DateTime extractedTime)
-        {
-            var taetiesTime = "America/Chicago";
-            var humanTime = "Australia/Melbourne";
-
-            LocalDateTime localDateTime = LocalDateTime.FromDateTime(extractedTime);
-            DateTimeZone opsTimeZone = DateTimeZoneProviders.Tzdb[taetiesTime];
-            ZonedDateTime opsZonedDateTime = localDateTime.InZoneLeniently(opsTimeZone);
-
-
-            DateTimeZone userTimeZone = DateTimeZoneProviders.Tzdb[humanTime];
-            ZonedDateTime userZonedDateTime = opsZonedDateTime.WithZone(userTimeZone);
-
-            return $"{extractedTime} in taeties time is {userZonedDateTime.ToDateTimeUnspecified()} in human time.";
-        }
-        #endregion
-
-
-
 
         #endregion
 
